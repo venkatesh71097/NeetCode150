7c291c3daff33bf35e17cb1e5c7234371eb6d72c
Venkatesh Bharadwaj Srinivasan <hcsclass10@gmail.com>
2026-05-19 21:34:20 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Route 1 - Counter: 
+        # from collections import Counter
+        # return Counter(s) == Counter(t)
+
+        # Route 2 - Dict tracking: 
+        # if len(s) != len(t):
+        #     return False
+        # d1 = {}
+        # d2 = {}
+        # for x, y in zip(s, t):
+        #     d1[x] = d1.get(x, 0) + 1
+        #     d2[y] = d2.get(y, 0) + 1
+        # return d1 == d2
+
+        # Route 3 - 
+        if len(s) != len(t):
+            return False
+        count = {}
+        for i in range(len(s)):
+            count[s[i]] = count.get(s[i], 0) + 1
+            count[t[i]] = count.get(t[i], 0) - 1
+        
+        for val in count.values():
+            if val != 0:
+                return False
+        return True
